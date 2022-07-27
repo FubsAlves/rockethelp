80f945db872bf1a4ca593b2b4b3aeda862a120b5
Fabiano Alves <fabianoxtx@gmail.com>
2022-07-26 22:38:47 -0300

    Component SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,18 @@
-import { VStack, Heading } from 'native-base';
+import { useState } from 'react';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
 import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn() {
+    const [name, setName] = useState('');
+
+    const [password, setPassword] = useState('');
+
+    const { colors } = useTheme();
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
@@ -12,7 +21,20 @@ export function SignIn() {
                 Acesse sua conta
             </Heading>
 
-            <Input />
+            <Input placeholder="E-mail"
+            mb={4}
+            InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} /> } ml={4} />}
+            onChangeText={setName}
+            />
+
+            <Input placeholder="Senha"
+            mb={8}
+            InputLeftElement={<Icon as={<Key color={colors.gray[300]} /> } ml={4} />}
+            secureTextEntry
+            onChangeText={setPassword}
+            />
+
+            <Button title="Entrar" w="full" />
         </VStack>
     )
 }
